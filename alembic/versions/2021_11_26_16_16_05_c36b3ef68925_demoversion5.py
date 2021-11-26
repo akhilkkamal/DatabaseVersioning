78c18ce05e1815b52b2bd5b2f701b7b68f674268
Akhil.Kamal <akhilkkamal@gmail.com>
2021-11-26 16:16:11 +0530

    demo

---
+++ alembic/versions/2021_11_26_16_16_05_c36b3ef68925_demoversion5.py
@@ -0,0 +1,28 @@
+"""Demoversion5
+
+Revision ID: c36b3ef68925
+Revises: 239731b3aedc
+Create Date: 2021-11-26 16:16:05.936706
+
+"""
+
+from os import getcwd
+from os.path import join
+
+import sqlalchemy as sa
+from alembic import context, op
+
+
+# revision identifiers, used by Alembic.
+revision = 'c36b3ef68925'
+down_revision = '239731b3aedc'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
